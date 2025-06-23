26aafa01c465f84a27e97e243c3224270d0b04ab
JavierRetana UFidelitas <jretana60848@ufide.ac.cr>
2025-06-23 15:01:01 -0600

    Add files via upload

---
+++ PracticaProgramada3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicaprogramada3;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author jareg
+ */
+public class PracticaProgramada3 {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new EmpleadoSupervisor().setVisible(true));
+    }
+}
